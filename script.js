9184b6d48f1302cf99a1fbc4828fced40763206d
GheeGhee2012 <108237136+GheeGhee2012@users.noreply.github.com>
2025-10-17 22:03:02 -0400

    f

---
+++ script.js
@@ -0,0 +1,39 @@
+const mainAccBtn = document.getElementById("mainAccBtn");
+const manageAccBtn = document.getElementById("manageAccBtn");
+
+let openedAccDropdown = false;
+
+function getCookie(name) {
+  const cookies = document.cookie.split('; ');
+  for (let c of cookies) {
+    const [key, val] = c.split('=');
+    if (key === name) return val;
+  }
+  return null;
+}
+
+let createdAccount = false;
+
+if (getCookie("username")) {
+    mainAccBtn.innerText = getCookie("username") + " >";
+    createdAccount = true;
+} else {
+    mainAccBtn.innerText = "Create Gravical Account";
+    let createdAccount = false;
+}
+
+function mainAccBtnOnClick() {
+    if (createdAccount) {
+        if (openedAccDropdown = false) {
+            manageAccBtn.classList.remove("hide");
+            manageAccBtn.innerText = getCookie("username") + " <";
+            openedAccDropdown = true;
+        } else {
+            manageAccBtn.classList.add("hide");
+            manageAccBtn.innerText = getCookie("username") + " >";
+            openedAccDropdown = true;
+        }
+    } else {
+        window.location.href = "https://gheeghee2012.github.io/manageaccount/index.html";
+    }
+}
